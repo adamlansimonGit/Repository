a1c9641297721cadaa7e337219ba9adc3f1d52a4
Adam Simon <adamlansimon@gmail.com>
2022-02-27 19:49:23 +0800

    demo commit

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('Adam simon')
+    print('Weng simon')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
